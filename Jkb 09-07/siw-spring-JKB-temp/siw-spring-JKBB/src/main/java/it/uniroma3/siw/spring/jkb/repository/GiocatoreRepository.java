59c2fbbff258b312c96508b4fa44bacffb2244c6
OlivieroC <72610655+OlivieroC@users.noreply.github.com>
2021-07-09 17:40:54 +0200

    commit ennesimo

--- Jkb 09-07/siw-spring-JKB-temp/siw-spring-JKBB/src/main/java/it/uniroma3/siw/spring/jkb/repository/GiocatoreRepository.java
+++
@@ -1,9 +0,0 @@
-package it.uniroma3.siw.spring.jkb.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import it.uniroma3.siw.spring.jkb.model.Giocatore;
-
-public interface GiocatoreRepository extends CrudRepository<Giocatore,Long> {
-
-}
--- Jkb 09-07/siw-spring-JKB-temp/siw-spring-JKBB/src/main/java/it/uniroma3/siw/spring/jkb/repository/IndirizzoRepository.java
+++
@@ -1,9 +0,0 @@
-package it.uniroma3.siw.spring.jkb.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import it.uniroma3.siw.spring.jkb.model.Indirizzo;
-
-public interface IndirizzoRepository  extends CrudRepository<Indirizzo,Long>{
-	
-}
